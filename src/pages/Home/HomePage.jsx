e215e63bce5564cdf33d57245ddf351de4af219c
DatNgo <ngominhdat110115@gmail.com>
2022-09-19 20:06:47 +0700

    landing page

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,8 +1,17 @@
-import { Container } from '@mui/system';
+import { Button } from '@mui/material';
 import React from 'react';
+import styles from './homepage.module.css';
 
 function HomePage() {
-  return <Container maxWidth="sm" sx={{ paddingTop: 5 }}></Container>;
+  return (
+    <div className={styles['container']}>
+      <div className={styles['banner']}>
+        <h1>E-working</h1>
+        <h3>Project management solution</h3>
+        <Button variant="contained">Get started</Button>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
