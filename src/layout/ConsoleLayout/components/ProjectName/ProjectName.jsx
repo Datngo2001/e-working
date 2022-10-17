1dc75f74fa4dbcbfb77ad3d4aa8e5bb9318e2ba2
DatNgo <ngominhdat110115@gmail.com>
2022-10-17 21:22:38 +0700

    add setting icon

--- src/layout/ConsoleLayout/components/ProjectName/ProjectName.jsx
+++ src/layout/ConsoleLayout/components/ProjectName/ProjectName.jsx
@@ -1,15 +1,21 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import SettingsIcon from '@mui/icons-material/Settings';
 import styles from './projectName.module.css';
 
 function ProjectName() {
   const { currentProject } = useSelector((state) => state.project);
   return (
     <div className={styles['container']}>
-      <Typography variant="h5" sx={{ textTransform: 'capitalize', wordWrap: 'normal' }}>
+      <Typography
+        variant="h5"
+        sx={{ textTransform: 'capitalize', wordWrap: 'normal', marginRight: 'auto' }}>
         {currentProject?.name}
       </Typography>
+      <IconButton>
+        <SettingsIcon />
+      </IconButton>
     </div>
   );
 }
